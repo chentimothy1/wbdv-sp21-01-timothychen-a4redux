0fd27c8b5e70e5a7761e82128adc8a31c3fee6fc
chentimothy1 <timothychen1@protonmail.com>
2021-03-09 17:44:37 -0800

    added services skeleton

--- src/services/course-service.js
+++ src/services/course-service.js
@@ -1,6 +1,16 @@
 const COURSES_URL = "https://wbdv-generic-server.herokuapp.com/api/timca4/courses"
 // const MODULES_URL = "https://wbdv-generic-server.herokuapp.com/api/jannunzi/modules"
 
+export const createCourse = (course) =>
+    fetch(COURSES_URL, {
+        method: 'POST',
+        body: JSON.stringify(course),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
 export const findAllCourses = () =>
     fetch(COURSES_URL)
         .then(response => response.json())
@@ -13,17 +23,6 @@ export const deleteCourse = (courseId) =>
 
 export const findCourseById = (courseId) =>
     fetch(`${COURSES_URL}/${courseId}`)
-        .then(response => response.json())
-
-
-export const createCourse = (course) =>
-    fetch(COURSES_URL, {
-        method: 'POST',
-        body: JSON.stringify(course),
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
         .then(response => response.json())
 
 export const updateCourse = (courseId, course) =>
@@ -39,7 +38,7 @@ export const updateCourse = (courseId, course) =>
 export default {
     findAllCourses,
     deleteCourse: deleteCourse,
-    createCourse, // same as createCourse: createCourse
+    createCourse,
     updateCourse: updateCourse,
     findCourseById
 }
