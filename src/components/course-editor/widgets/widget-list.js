f821e9b01914dc46b6237d4a43dadec7ff16e85d
chentimothy1 <timothychen1@protonmail.com>
2021-03-21 14:33:47 -0700

    formatting

--- src/components/course-editor/widgets/widget-list.js
+++ src/components/course-editor/widgets/widget-list.js
@@ -1,63 +1,131 @@
 import React, { useEffect, useState } from "react";
 
 import { connect } from 'react-redux'
-import EditableItem from "../../editable-item";
+// implement EditableItem later
+// import EditableItem from "../../editable-item";
 import { useParams } from "react-router-dom";
-import TopicService from "../../../services/topic-service"
 import HeadingWidget from "./heading-widget";
 import ParagraphWidget from "./paragraph-widget";
 import widgetService from "../../../services/widget-service"
 
 const WidgetList = (
     {
-        widgets=[],
+        widgets = [],
         findWidgetsForTopic,
         createWidget,
         updateWidget,
         deleteWidget,
-    }) => {} 
+    }) => {
+
+    const [editingWidget, setEditingWidget] = useState({})
+
+    const { topicId } = useParams();
+
+    const wListDeleteWidget = (widget) => {
+        deleteWidget(widget);
+        setEditingWidget({});
+    };
+
+    const wListUpdateWidget = (widget) => {
+        console.log(widget)
+        updateWidget(widget);
+        setEditingWidget({});
+        findWidgetsForTopic(topicId);
+    };
+
+    const wListEditWidget = () => { };
 
     useEffect(() => {
-        findWidgetsForTopic()
-    }, [])
+        findWidgetsForTopic(topicId);
+    }, [topicId])
 
-
-
-    return(
-        <div className="mda-widget-window">
+    return (
+        <div>
+            <h2>Widgets <i className="btn btn-primary fas fa-plus" onClick={() => createWidget(topicId)}></i></h2>
             <ul className="list-group">
-                {widgets.map(widget =>
-                <li className="list-group-item"
-                    key={widget.id}>
-                    {
-                        widget.type === "HEADING" &&
-                            <HeadingWidget widget={widget}/>
-                    }
-                    {
-                        widget.type === "PARAGRAPH" &&
-                        <ParagraphWidget widget={widget}/>
-                    }
-                </li>
-                )}
+                {
+                    widgets.map(w =>
+                        <li key={w.id} className="list-group-item">
+
+                            {/* displays edit and delete */}
+                            <div>
+                                {
+                                    editingWidget.id === w.id &&
+                                    <>
+                                        <i onClick={() => wListUpdateWidget(editingWidget)} className="fas fa-2x fa-check float-right"></i>
+                                        <i onClick={() => wListDeleteWidget(w)} className="d-inline fas fa-2x fa-trash float-right"></i>
+                                    </>
+                                }
+                                {
+                                    editingWidget.id !== w.id &&
+                                    <>
+                                        <i onClick={() => setEditingWidget(w)} className="fas fa-2x fa-cog float-right"></i>
+                                    </>
+                                }
+                            </div>
+
+
+                            {/* show widgets */}
+                            <div>
+                                {w.type === "HEADING" &&
+                                    <HeadingWidget
+                                        widget={w}
+                                        editing={editingWidget.id === w.id}
+                                        editingWidget={editingWidget}
+                                        setEditingWidget={setEditingWidget} />}
+                                {w.type === "PARAGRAPH" &&
+                                    <ParagraphWidget
+                                        widget={w}
+                                        editing={editingWidget.id === w.id}
+                                        editingWidget={editingWidget}
+                                        setEditingWidget={setEditingWidget} />}
+                            </div>
+                        </li>
+                    )
+                }
             </ul>
         </div>
     )
 }
 
-const stpm = (state) => {
-    return {
-        widgets: state.widgetReducer.widgets
-    }
-}
+const stpm = (state) => ({
+    widgets: state.widgetReducer.widgets,
+})
 
 const dtpm = (dispatch) => ({
-    findAllWidgets: () => {
-        widgetService.findAllWidgets()
-            .then(widgets => dispatch({
-                type: "FIND_ALL_WIDGETS",
-                widgets: widgets
+    findWidgetsForTopic: (topicId) => {
+        widgetService.findWidgetsForTopic(topicId)
+            .then(theWidgets => dispatch({
+                type: "FIND_WIDGETS_FOR_TOPIC",
+                widgets: theWidgets,
+            }))
+    },
+    createWidget: (topicId) => {
+        widgetService.createWidget(topicId,
+            {
+                text: "New Widget",
+                type: "HEADING",
+                size: 1,
+            })
+            .then(theActualWidget => dispatch({
+                type: "CREATE_WIDGET",
+                widget: theActualWidget,
+            }))
+    },
+    deleteWidget: (widgetToDelete) => {
+        widgetService.deleteWidget(widgetToDelete.id)
+            .then(status => dispatch({
+                type: "DELETE_WIDGET",
+                widgetToDelete: widgetToDelete,
+            }))
+    },
+    updateWidget: (widgetToUpdate) => {
+        widgetService.updateWidget(widgetToUpdate)
+            .then(status => dispatch({
+                type: "UPDATE_WIDGET",
+                widgetToUpdate: widgetToUpdate,
             }))
     }
 })
 
-export default connect(stpm, dtpm)(WidgetList)
+export default connect(stpm, dtpm)(WidgetList);
--- src/reducers/widget-reducer.js
+++ src/reducers/widget-reducer.js
@@ -1,52 +1,62 @@
 const initialState = {
     widgets: [
-      { title: 'Test Topic 1', _id: '123', type: "1", text: "testing" },
-      { title: 'Test Topic 2', _id: '987', type: "2", text: "testing text" },
-      { title: 'Test Topic 3', _id: '456', type: "3", text: "this is texting text" },
+        { title: 'Test Topic 1', _id: '123', type: "1", text: "testing" },
+        { title: 'Test Topic 2', _id: '987', type: "2", text: "testing text" },
+        { title: 'Test Topic 3', _id: '456', type: "3", text: "this is texting text" },
     ]
-  }
+}
 
-const widgetReducer = (state=initialState, action) => {
+const widgetReducer = (state = initialState, action) => {
     switch (action.type) {
+
         case "CREATE_WIDGET":
-            return {
+            const newStateCreated = {
                 ...state,
                 widgets: [
                     ...state.widgets,
-                    action.widgets
+                    action.widget,
                 ]
             }
-        case "FIND_WIDGETS_FOR_TOPIC":
-            return {
-                ...state,
-                widgets: action.widgets
-            }
+            return newStateCreated;
+
         case "UPDATE_WIDGET":
             return {
-                ...state,
                 widgets: state.widgets.map(w => {
-                    if (w.id === action.updatedWidget.id) {
-                        return action.updatedWidget
+                    if (w.id === action.widgetToUpdate.id) {
+                        return action.widgetToUpdate;
                     } else {
-                        return w
-                    }
-                })
-            }
-        case "DELETE_WIDGET":
-            return {
-                ...state,
-                widgets: state.widgets.filter(w => {
-                    if (w.id !== action.deletedWidget.id) {
-                        return true
-                    } else {
-                        return false
+                        return w;
                     }
                 })
             }
 
+        case "DELETE_WIDGET":
+            const newStateDeleted = {
+                widgets: state.widgets.filter(w => {
+                    if (w.id !== action.widgetToDelete.id) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                })
+            }
+            return newStateDeleted;
+
+        case "FIND_WIDGETS_FOR_TOPIC":
+            return {
+                widgets: action.widgets,
+            }
+        
+        //below dont really do anything
+        case "FIND_ALL_WIDGETS":
+            return state;
+
+        case "FIND_WIDGET":
+            return state;
+
         default:
-            return state
+            return state;
     }
 }
 
-export default widgetReducer
+export default widgetReducer;
