9d8609af3d7dfb3f538300e549adb6324aa48650
chentimothy1 <timothychen1@protonmail.com>
2021-04-01 15:16:49 -0700

    updated image widget with width and height

--- src/components/course-editor/widgets/image-widget.js
+++ src/components/course-editor/widgets/image-widget.js
@@ -13,27 +13,41 @@ const ImageWidget = (
             {
                 editing &&
                 <div style={{ "width": "85%" }}>
-                    <select
-                        //widget type selector
-                        //add more widgets here later
-                        className="m-2 form-control"
-                        onChange={(e) => setEditingWidget({ ...editingWidget, type: e.target.value })}
-                        value={editingWidget.type}>
-                        <option value={"HEADING"}>Heading</option>
-                        <option value={"PARAGRAPH"}>Paragraph</option>
-                        <option value={"LIST"}>List</option>
-                        <option value={"IMAGE"}>Image</option>
-                    </select>
 
-                    <div>
-                        URL
-                        <input value={widget.url} className="form-control" />
-                        width
-                        <input value={widget.width} className="form-control" />
-                        height
-                        <input value={widget.height} className="form-control" />
-                    </div>
+                    {/* URL */}
+                    <h2 className="m-2">Image URL:</h2>
+                    <input
+                        className="m-2 form-control"
+                        onChange={(e) => setEditingWidget({ ...editingWidget, urlRef: e.target.value })}
+                        value={editingWidget.urlRef}>
+                    </input>
+
+                    {/* Width */}
+                    <h2 className="m-2">Image Width:</h2>
+                    <input
+                        className="m-2 form-control"
+                        onChange={(e) => setEditingWidget({ ...editingWidget, width: e.target.value })}
+                        value={editingWidget.width}>
+                    </input>
+
+                    {/* Height */}
+                    <h2 className="m-2">Image Height:</h2>
+                    <input
+                        className="m-2 form-control"
+                        onChange={(e) => setEditingWidget({ ...editingWidget, height: e.target.value })}
+                        value={editingWidget.height}>
+                    </input>
+
                 </div>
+            }
+            {
+                !editing &&
+                <>
+                    <img src={widget.urlRef} alt="img"
+                        width={widget.width}
+                        height={widget.height}
+                    />
+                </>
             }
         </>
     )
